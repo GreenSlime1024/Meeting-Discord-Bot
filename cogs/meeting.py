665181c5f400ab6f5b00c2eeb44ae450ab1c44ee
greenslime1024 <greenslime1024@gmail.com>
2023-06-03 07:29:32 +0000

    mongodb

--- cogs/meeting.py
+++ cogs/meeting.py
@@ -11,26 +11,28 @@ import uuid
 import asyncio
 from discord.ui import Button, Select, View
 from utils.meeting import Meeting
+from bot import MyBot
+import pymongo
 
 
 class MeetingCommand(commands.Cog):
-    def __init__(self, bot):
+    def __init__(self, bot: MyBot):
         self.bot = bot
-
+        self.mongo_client = bot.mongo_client
+        self.settings_db = bot.settings_db
+        self.server_settings_coll = bot.server_settings_coll
+        
     # discord create meeting command
     @app_commands.command(name="create_meeting", description="create a meeting")
     @app_commands.describe(title="title of the meeting", hour="hour that meeting will starts at (24-hour)", minute="minute that meeting will starts at", day="day that meeting will starts at", month="month that meeting will starts at", year="year that meeting will starts at", role="members of the role that are asked to join the meeting")
     async def create_meeting(self, interaction: discord.Interaction, title: str, hour: int, minute: int, role: discord.Role = None, day: int = None, month: int = None, year: int = None):
 
-    # check if the user has set guild settings
-        with open("guilds_info.json", mode="r", encoding="utf8") as jfile:
-            jdata = json.load(jfile)
-        guild_ID = interaction.guild.id
-        try:
-            timezone: str = jdata[str(guild_ID)]["timezone"]
-        except KeyError:
-            await error.error_message(interaction, error="guild setting not found", description="You haven't set server settings.\nPlease use </set_server_settings:1072440724118847554> to set.")
-            return 
+        # check if the user has set guild settings
+        server_settings = self.server_settings_coll.find_one({"guild_id": interaction.guild.id})
+        if server_settings == None:
+            await error.error_message(interaction, error="guild settings not set", description="Please set guild settings first.")
+            return
+        timezone = server_settings["timezone"]
         
         # auto fill the time if user didn't type
         now_time_UTC = datetime.datetime.now(datetime.timezone.utc).replace(second=0, microsecond=0)
@@ -65,6 +67,54 @@ class MeetingCommand(commands.Cog):
         meeting = Meeting(self.bot, interaction, title, hour, minute, timezone, role, day, month, year)
         # create meeting
         await meeting.create_meeting()
+    
+    @app_commands.command(name="set_server_settings", description="set server settings")
+    @app_commands.describe(timezone="choose your timezone")
+    @app_commands.choices(timezone=[
+        discord.app_commands.Choice(name="GMT+0", value="Etc/GMT-0"),
+        discord.app_commands.Choice(name="GMT+1", value="Etc/GMT-1"),
+        discord.app_commands.Choice(name="GMT+2", value="Etc/GMT-2"),
+        discord.app_commands.Choice(name="GMT+3", value="Etc/GMT-3"),
+        discord.app_commands.Choice(name="GMT+4", value="Etc/GMT-4"),
+        discord.app_commands.Choice(name="GMT+5", value="Etc/GMT-5"),
+        discord.app_commands.Choice(name="GMT+6", value="Etc/GMT-6"),
+        discord.app_commands.Choice(name="GMT+7", value="Etc/GMT-7"),
+        discord.app_commands.Choice(name="GMT+8", value="Etc/GMT-8"),
+        discord.app_commands.Choice(name="GMT+9", value="Etc/GMT-9"),
+        discord.app_commands.Choice(name="GMT+10", value="Etc/GMT-10"),
+        discord.app_commands.Choice(name="GMT+11", value="Etc/GMT-11"),
+        discord.app_commands.Choice(name="GMT+12", value="Etc/GMT-12"),
+        discord.app_commands.Choice(name="GMT-11", value="Etc/GMT+11"),
+        discord.app_commands.Choice(name="GMT-10", value="Etc/GMT+10"),
+        discord.app_commands.Choice(name="GMT-9", value="Etc/GMT+9"),
+        discord.app_commands.Choice(name="GMT-8", value="Etc/GMT+8"),
+        discord.app_commands.Choice(name="GMT-7", value="Etc/GMT+7"),
+        discord.app_commands.Choice(name="GMT-6", value="Etc/GMT+6"),
+        discord.app_commands.Choice(name="GMT-5", value="Etc/GMT+5"),
+        discord.app_commands.Choice(name="GMT-4", value="Etc/GMT+4"),
+        discord.app_commands.Choice(name="GMT-3", value="Etc/GMT+3"),
+        discord.app_commands.Choice(name="GMT-2", value="Etc/GMT+2"),
+        discord.app_commands.Choice(name="GMT-1", value="Etc/GMT+1"),
+    ])
+    
+    async def set_server_settings(self, interaction: discord.Interaction, timezone: discord.app_commands.Choice[str], category: discord.CategoryChannel):
+        server_settings = {
+                "guild_id": interaction.guild.id,
+                "timezone": timezone.value,
+                "category_ID": category.id
+            }
+        if self.server_settings_coll.find_one({"guild_id": interaction.guild.id}) == None:
+            self.server_settings_coll.insert_one(server_settings)
+        else:
+            self.server_settings_coll.update_one({"guild_id": interaction.guild.id}, {"$set": server_settings})
+        await interaction.response.send_message(f"guild info set.", ephemeral=False)
+
+
+    @app_commands.command(name="test", description="test")
+    @commands.is_owner()
+    async def test(self, interaction: discord.Interaction):
+        print(self.mongo_client.list_database_names())
+        await interaction.response.send_message("test", ephemeral=True)
 
 async def setup(bot):
     await bot.add_cog(MeetingCommand(bot))
