4f777620bca2250dc875ace9f326b85de017cc42
greenslime1024 <greenslime1024@gmail.com>
2023-06-03 07:27:45 +0000

    optimize

--- cogs/activity.py
+++ cogs/activity.py
@@ -13,12 +13,10 @@ class Activity(commands.Cog):
 
         async def activiy_task():
             await self.bot.wait_until_ready()
-            while not self.bot.is_closed():
-                activitys = ["運作平台: Eri24816 租的 server",
-                             "Made By GreenSlime1024", "還沒做好"]
-                for i in activitys:
-                    await self.bot.change_presence(activity=discord.Game(i))
-                    await asyncio.sleep(15)
+            activitys = ["運作平台: Eri24816 租的 server", "Made By GreenSlime1024", "還沒做好"]
+            for i in activitys:
+                await self.bot.change_presence(activity=discord.Game(i))
+                await asyncio.sleep(15)
 
         self.bg_task = self.bot.loop.create_task(activiy_task())
 
