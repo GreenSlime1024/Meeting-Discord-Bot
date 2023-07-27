ab8415bd7166f0273993f090475a955604d55b62
greenslime1024 <greenslime1024@gmail.com>
2023-07-27 04:23:37 +0000

    fix activity can't change

--- cogs/activity.py
+++ cogs/activity.py
@@ -1,24 +1,26 @@
 import discord
-from discord.ext import commands
+from discord.ext import commands, tasks
 import asyncio
+from bot import MyBot
 
 
 class Activity(commands.Cog):
     @commands.Cog.listener()
     async def on_ready(self):
         print("Activity cog loaded.")
+        self.activity_task.start()
 
-    def __init__(self, bot):
+    def __init__(self, bot: MyBot):
         self.bot = bot
+        self.activitys = ["運作平台: Eri24816 租的 server", "Made By GreenSlime1024", "核心功能已完成", "但還有細節和新功能待開發"]
+        self.index = 0
 
-        async def activiy_task():
-            await self.bot.wait_until_ready()
-            activitys = ["運作平台: Eri24816 租的 server", "Made By GreenSlime1024", "核心功能已完成", "但還有細節和新功能待開發"]
-            for activity in activitys:
-                await self.bot.change_presence(activity=discord.Game(activity))
-                await asyncio.sleep(15)
-                
-        self.bot.loop.create_task(activiy_task())
+    @tasks.loop(seconds=15)
+    async def activity_task(self):
+        await self.bot.change_presence(activity=discord.Game(name=self.activitys[self.index]))
+        self.index += 1
+        if self.index == len(self.activitys)-1:
+            self.index = 0
 
 async def setup(bot):
     await bot.add_cog(Activity(bot))
