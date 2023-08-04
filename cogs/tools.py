bb35178d1b6ac5cfc34854bd05615a2a247dbe19
greenslime1024 <greenslime1024@gmail.com>
2023-08-04 08:52:58 +0000

    add help command

--- cogs/tools.py
+++ cogs/tools.py
@@ -1,15 +1,34 @@
 import discord
 from discord.ext import commands
 from discord import app_commands
+from bot import MyBot
 
 
 class Main(commands.Cog):
-    def __init__(self, bot):
+    def __init__(self, bot: MyBot):
         self.bot = bot
         
     @commands.Cog.listener()
     async def on_ready(self):
         print("Main cog loaded.")
+
+    @app_commands.command(name="help", description="check help")
+    async def help(self, interaction: discord.Interaction):
+        tools_embed = discord.Embed(title="Tools Command", color=discord.Color.blue())
+        tools_embed.add_field(name="ping", value="check ping", inline=False)
+        tools_embed.add_field(name="author", value="check author's blog", inline=False)
+        tools_embed.add_field(name="github-repo", value="check this bot's github repo", inline=False)
+        tools_embed.add_field(name="support-server", value="check this bot's support server", inline=False)
+        tools_embed.add_field(name="guild", value="check the guilds where I am in", inline=False)
+        
+        meeting_embed = discord.Embed(title="Meeting Command", color=discord.Color.blue())
+        meeting_embed.add_field(name="</set_server_settings:1121308554448617602>", value="set server settings\nYou must do this before you </create_meeting:1101520045449957467>", inline=False)
+        meeting_embed.add_field(name="</create_meeting:1101520045449957467>", value="create a meeting", inline=False)
+        
+
+        embeds = [tools_embed, meeting_embed]
+        await interaction.response.send_message(embeds=embeds)
+        
 
     @app_commands.command(name="ping", description="check ping")
     async def ping(self, interaction: discord.Interaction):
@@ -27,9 +46,9 @@ class Main(commands.Cog):
     async def support_server(self, interaction: discord.Interaction):
         await interaction.response.send_message("https://discord.gg/CVUjGJT94b")
 
-    @app_commands.command(name="guild", description="check the guilds where I provide services")
+    @app_commands.command(name="guild", description="check the guilds where I am in")
     async def guild(self, interaction: discord.Interaction):
-        embed = discord.Embed(title="Server List", color=0x8280ff)
+        embed = discord.Embed(title="Server List", color=discord.Color.blue())
         for guild in self.bot.guilds:
             embed.add_field(name=guild, value=guild.id, inline=False)
         await interaction.response.send_message(embed=embed)
