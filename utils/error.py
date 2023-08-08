63352895b6a3482e4d00c7925efb687d61d73993
greenslime1024 <greenslime1024@gmail.com>
2023-08-08 06:37:20 +0000

    add mode parameter

--- utils/error.py
+++ utils/error.py
@@ -1,7 +1,12 @@
 import discord
 
 class error():
-    async def error_message(interaction, error, description = None, ephemeral=True):
+    async def error_message(interaction, error, description:str="no info", mode:str="respond",ephemeral:bool=True):
         embed=discord.Embed(title="Error!", description=f"```{error}```", color=0xff0000)
         embed.add_field(name="description", value=description, inline=False)
-        await interaction.response.send_message(embed=embed, ephemeral=ephemeral)
+        if mode == "respond":
+            await interaction.response.send_message(embed=embed, ephemeral=ephemeral)
+        elif mode == "edit":
+            await interaction.response.edit_message(embed=embed)
+        elif mode == "follow":
+            await interaction.followup.send(embed=embed, ephemeral=ephemeral)
